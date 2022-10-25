d0a3b18454773222ee34b77d3aca591cf2ead2c1
jongin12 <jongin981224@gmail.com>
2022-10-25 11:52:07 +0900

    route 모듈화

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import http from "http";
 import fs from "fs";
-import API from "./LOL_API.js";
-import ejs from "ejs";
+import summonerPage from "./module/Route/summoner.js";
 // ejs.render(경로, 데이터, 옵션);
 
 const server = http.createServer((req, res) => {
@@ -16,35 +15,7 @@ const server = http.createServer((req, res) => {
 
   if (req.url.startsWith("/summoner/")) {
     const name = req.url.split("/")[2];
-    let apiData = async function () {
-      let list = {};
-      list.summoner = await API.summoners(name);
-      if (list.summoner.status) {
-        if (list.summoner.status.status_code === 403) {
-          res.end("Riot Key error");
-        } else if (list.summoner.status.status_code === 404) {
-          res.end("없는 소환사명입니다");
-        }
-      } else {
-        list.league = await API.summonersLeague(list.summoner.id);
-        list.matchList = await API.matchList(list.summoner.puuid);
-        for (let i = 0; i < list.matchList.length; i++) {
-          list.matchData[i] = await API.matchInfo(list.matchList[i]);
-        }
-        fs.readFile("html/summoner.ejs", "utf8", function (err, data) {
-          res.writeHead(200, { "Content-Type": "text/html" });
-          res.end(
-            ejs.render(data, {
-              summoner: list.summoner,
-              league: list.league,
-              matchList: list.matchList,
-              matchData: list.matchData,
-            })
-          );
-        });
-      }
-    };
-    apiData();
+    summonerPage(name, res);
   }
 });
 
--- module/Route/summoner.js
+++ module/Route/summoner.js
@@ -1,3 +1,34 @@
-import API from "./LOL_API.js";
+import API from "../../LOL_API.js";
 import fs from "fs";
 import ejs from "ejs";
+
+let summonerPage = async function (name, res) {
+  let list = {};
+  list.summoner = await API.summoners(name);
+  if (list.summoner.status) {
+    if (list.summoner.status.status_code === 403) {
+      res.end("Riot Key error");
+    } else if (list.summoner.status.status_code === 404) {
+      res.end("없는 소환사명입니다");
+    }
+  } else {
+    list.league = await API.summonersLeague(list.summoner.id);
+    list.matchList = await API.matchList(list.summoner.puuid);
+    for (let i = 0; i < list.matchList.length; i++) {
+      list.matchData[i] = await API.matchInfo(list.matchList[i]);
+    }
+    fs.readFile("html/summoner.ejs", "utf8", function (err, data) {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(
+        ejs.render(data, {
+          summoner: list.summoner,
+          league: list.league,
+          matchList: list.matchList,
+          matchData: list.matchData,
+        })
+      );
+    });
+  }
+};
+
+export default summonerPage;
