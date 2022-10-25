3fd573a6718d453b0a8fea4055c3b8a660fcbc20
jongin12 <jongin981224@gmail.com>
2022-10-25 16:46:29 +0900

    app.js 주석, queueId 추가, summoner page 작업 중

--- app.js
+++ app.js
@@ -11,32 +11,39 @@ const server = http.createServer((req, res) => {
   const main = fs.readFileSync("./html/index.html", (err) => {
     if (err) throw err;
   });
+
   if (req.url === "/") {
     res.writeHead(200, { "Content-Type": "text/html; charset= utf-8" });
     res.end(main);
-  } else if (req.url.startsWith("/summoner/")) {
+  } // main page
+  else if (req.url.startsWith("/summoner/")) {
     const name = req.url.split("/")[2];
     summonerPage(name, res);
-  } else if (req.url.startsWith("/match/")) {
+  } // user page
+  else if (req.url.startsWith("/match/")) {
     const matchId = req.url.split("/")[2];
     matchPage(matchId, res);
-  } else if (req.url.startsWith("/active/")) {
+  } // match page
+  else if (req.url.startsWith("/active/")) {
     const name = req.url.split("/")[2];
     activePage(name, res);
-  } else if (req.url.match(".css")) {
+  } // activegame page
+  else if (req.url.endsWith(".css")) {
     let cssPath = path.join(__dirname, "html", req.url);
     let fileStream = fs.createReadStream(cssPath, "utf-8");
     res.writeHead(200, { "Content-Type": "text/css" });
     fileStream.pipe(res);
-  } else if (req.url.match(".png")) {
+  } // css file
+  else if (req.url.endsWith(".png")) {
     let imagePath = path.join(__dirname, "html", req.url);
     let fileStream = fs.createReadStream(imagePath);
     res.writeHead(200, { "Content-Type": "image/png" });
     fileStream.pipe(res);
-  } else {
+  } // png file
+  else {
     res.writeHead(404, { "Content-Type": "text/html" });
     res.end("404 Not Found");
-  }
+  } // 404 err page
 });
 
 server.listen(5050, () => {
--- module/Route/summoner.js
+++ module/Route/summoner.js
@@ -1,6 +1,15 @@
-import API from "../../LOL_API.js";
 import fs from "fs";
 import ejs from "ejs";
+import API from "../../LOL_API.js";
+import queueId from "../queueId.js";
+
+const stringCheck = (string1, string2) => {
+  let small_1 = string1.toLowerCase();
+  let noSpace_1 = small_1.split(" ").join("");
+  let small_2 = string2.toLowerCase();
+  let noSpace_2 = small_2.split(" ").join("");
+  return noSpace_1 === noSpace_2;
+};
 
 let summonerPage = async function (name, res) {
   let list = { matchData: [] };
@@ -17,7 +26,58 @@ let summonerPage = async function (name, res) {
     list.matchList = await API.matchList(list.summoner.puuid);
     for (let i = 0; i < list.matchList.length; i++) {
       list.matchData[i] = await API.matchInfo(list.matchList[i]);
+      for (let j = 0; j < 10; j++) {
+        let summonerName = list.matchData[i].info.participants[j].summonerName;
+        if (stringCheck(summonerName, list.summoner.name)) {
+          list.matchData[i].myNum = j;
+        }
+      }
+      let myNum = list.matchData[i].myNum;
+      let myData = list.matchData[i].info.participants[myNum];
+      //내 번호 찾기
+
+      let now = new Date();
+      let endtime = list.matchData[i].info.gameEndTimestamp;
+      let value = now - endtime;
+      let hour = value / 1000 / 60 / 60;
+      let time = "";
+      if (hour >= 24) {
+        time = Math.floor(hour / 24);
+        time = time + "일 전";
+      } else if (24 > hour && hour >= 1) {
+        time = Math.floor(hour);
+        time = time + "시간 전";
+      } else if (1 > hour) {
+        time = Math.floor(hour * 60);
+        time = time + "분 전";
+      }
+      list.matchData[i].myTime = time;
+      // 몇시간전 경기인지 ~
+
+      let playtime = list.matchData[i].info.gameDuration;
+      let min = Math.floor(playtime / 60);
+      let sec = playtime % 60;
+      list.matchData[i].myGameDuration = `${min}분 ${sec}초`;
+      // 플레이시간
+
+      let queueNum = list.matchData[i].info.queueId;
+      let queueId_KR = queueId[queueNum];
+      list.matchData[i].queueId_KR = queueId_KR;
+      // 게임 모드
+
+      if (myData.deaths === 0) {
+        list.matchData[i].kdaText = "perfect";
+      } else {
+        let kda = (myData.kills + myData.assists) / myData.deaths;
+        list.matchData[i].kdaText = kda.toFixed(2);
+      }
+      // KDA
     }
+
+    // const json = JSON.stringify(list);
+    // res.writeHead(200, { "Content-Type": "charset=utf-8" });
+    // res.end(json);
+
     fs.readFile("html/summoner.ejs", "utf8", function (err, data) {
       res.writeHead(200, { "Content-Type": "text/html" });
       res.end(
